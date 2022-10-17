aa3b35046fbed618bad10f76c1d59f4d2cee5517
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-10-17 09:27:14 +0300

    added NodeJs clustering

--- helloNodeJs/index.js
+++ helloNodeJs/index.js
@@ -1,11 +1,29 @@
 import express from "express";
+import cluster from "cluster";
+import process from "process";
 
-const app = express();
+// assuming have 8 CPUs
+// 1 CPU for the primary , and the rest for the forks/workers
+const numCPUs = 7;
 
-app.get("/hello", (req, res) => {
-  res.send("Hello!");
-});
+if (cluster.isPrimary) {
+  console.log(`Primary started pid: ${process.pid}`);
 
-app.listen(8080, () => {
-  console.log("App started ");
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker id: ${cluster.worker.id} died`);
+  });
+} else {
+  const app = express();
+
+  app.get("/hello", (req, res) => {
+    res.send("Hello!");
+  });
+
+  app.listen(8080, () => {
+    console.log(`App started in worker pid: ${process.pid} id: ${cluster.worker.id}`);
+  });
+}
